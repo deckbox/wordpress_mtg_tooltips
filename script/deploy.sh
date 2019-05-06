18ac523e1e9bcaf0faf49ae8675834e58520c6c1
Sebastian Zaha <sebastian.zaha@gmail.com>
2019-05-06 21:35:05 +0200

    Update deploy script

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,4 +3,4 @@
 # The brilliant wp_release deploy script we use is made by Olof Montin:
 #   https://github.com/spurge/wordpress-plugin-git-svn/tree/e41c818def53ae9bf02ad2873e5389ee1612266f 
 
-wp_release -n magic-the-gathering-card-tooltips -s http://plugins.svn.wordpress.org/magic-the-gathering-card-tooltips/ -g git@github.com:SebastianZaha/wordpress_mtg_tooltips.git
+bash wp_release -n magic-the-gathering-card-tooltips -s http://plugins.svn.wordpress.org/magic-the-gathering-card-tooltips/ -g https://github.com/SebastianZaha/wordpress_mtg_tooltips.git
