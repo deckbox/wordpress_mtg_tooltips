f991c68f01aa924f28fff2b49ce476b4fb4ec6fd
Sebastian Zaha <sebastian.zaha@gmail.com>
2025-01-22 15:17:55 +0100

    Allow deploy script to run from repo root.

--- script/run.sh
+++ script/run.sh
@@ -3,7 +3,7 @@
 deploy() {
   # The brilliant wp_release deploy script we use is made by Olof Montin:
   #   https://github.com/spurge/wordpress-plugin-git-svn/tree/e41c818def53ae9bf02ad2873e5389ee1612266f
-  bash wp_release -n magic-the-gathering-card-tooltips -s http://plugins.svn.wordpress.org/magic-the-gathering-card-tooltips/ -g https://github.com/SebastianZaha/wordpress_mtg_tooltips.git
+  bash "$(dirname "$0")/wp_release" -n magic-the-gathering-card-tooltips -s http://plugins.svn.wordpress.org/magic-the-gathering-card-tooltips/ -g https://github.com/SebastianZaha/wordpress_mtg_tooltips.git
 }
 
 copy_to_wp() {
